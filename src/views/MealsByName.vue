e62fe2c79b0f48d07a92cfb4dfced291e7adb887
BloodShop <koliakovcr7@gmail.com>
2023-03-01 15:29:15 +0200

    init meals project

---
+++ src/views/MealsByName.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="p-8">
+        <input
+            type="text"
+            v-model="keyword"
+            class="rounded border-2 border-gray-200 w-full"
+            placeholder="Search for Meals"
+            @change="searchMeals"
+        />
+    </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+import axiosClient from "../axiosClient";
+import store from "../store";
+
+const meals = computed(() => store.state.)
+const keyword = ref("");
+function searchMeals() {
+    store.dispatch("searchMeals", keyword.value);
+}
+</script>
