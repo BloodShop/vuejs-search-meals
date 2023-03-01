476cdb9d790f29c2e074037eab3dcd6b8dc30ea4
BloodShop <koliakovcr7@gmail.com>
2023-03-01 15:55:39 +0200

    meals by name search

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,14 +8,46 @@
             @change="searchMeals"
         />
     </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div
+            v-for="meal of meals"
+            :key="meal.idMeal"
+            class="bg-white shadow rounded-xl"
+        >
+            <img
+                :src="meal.strMealThumb"
+                alt="meal.strMeal"
+                class="rounded-t-xl w-full h-48 object-cover"
+            />
+            <div class="p-3">
+                <h3 class="font-semibold">{{ meal.strMeal }}</h3>
+                <p class="mb-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+                    aspernatur, non ex aliquam nobis assumenda.
+                </p>
+                <div class="flex items-center justify-between">
+                    <a
+                        :href="meal.strYoutube"
+                        target="_blank"
+                        class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+                        >Youtube</a
+                    >
+                    <router-link
+                        class="px-3 py-2 rounded border-2 text-white border-purple-600 bg-purple-500 hover:bg-purple-600 transition-colors"
+                        to="/"
+                        >View
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { computed, ref } from "vue";
-import axiosClient from "../axiosClient";
 import store from "../store";
 
-const meals = computed(() => store.state.)
+const meals = computed(() => store.state.searchedMeals);
 const keyword = ref("");
 function searchMeals() {
     store.dispatch("searchMeals", keyword.value);
